cc3dedb12cba238e45bcc1f577d05f514d0482dd
Tharushi Abeysinghe <tharushi8geethma@gmail.com>
2026-03-03 13:17:22 +1100

    chore: enhance UI and format code

--- src/app/listing-form/listing-form.ts
+++ src/app/listing-form/listing-form.ts
@@ -2,51 +2,51 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Listing } from '../types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listing-form',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './listing-form.html',
   styleUrl: './listing-form.css',
 })
 export class ListingForm implements OnInit {
-    @Input() componentMessage: string = '';
-    @Input() buttonMessage: string = '';
-    @Input() currentListing?: Listing;
-    @Output() messageEvent = new EventEmitter<Listing>();
-
+  @Input() componentMessage: string = '';
+  @Input() buttonMessage: string = '';
+  @Input() currentListing?: Listing;
+  @Output() messageEvent = new EventEmitter<Listing>();
 
   id: string = '';
   name: string = '';
   description: string = '';
   price: string = '';
-  views: number=0;
+  views: number = 0;
+  user_id: string = "";
 
-  constructor(){
+  constructor() {
   }
 
   ngOnInit(): void {
-    if(this.currentListing !== undefined){
+    if (this.currentListing !== undefined) {
       this.id = this.currentListing.id;
       this.name = this.currentListing.name;
       this.description = this.currentListing.description;
-      this.price=this.currentListing.price.toString();
-    }else{
-      console.log("nothing..");
+      this.price = this.currentListing.price.toString();
+      this.views = this.currentListing.views;
+      this.user_id = this.currentListing.user_id;
     }
 
   }
 
 
   onSubmit(): void {
-
-    const currentListing:Listing = {
+    const currentListing: Listing = {
       id: this.id,
-      name:this.name,
+      name: this.name,
       description: this.description,
       price: Number(this.price),
-      user_id:"",
-      views:0
+      user_id: this.user_id,
+      views: this.views
     }
 
     this.messageEvent.emit(currentListing);
