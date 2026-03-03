6ee036bd2a3a7e43d1e42072acd90f98a6d513ad
Tharushi Abeysinghe <tharushi8geethma@gmail.com>
2026-03-03 13:05:11 +1100

    feat: add about-us component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { MyListings } from './my-listings/my-listings';
 import { EditListing } from './edit-listing/edit-listing';
 import { NewListing } from './new-listing/new-listing';
 import { Home } from './home/home';
+import { AboutUs } from './about-us/about-us';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path:'home', component:Home},
+    {path:'about-us', component:AboutUs},
     {path: 'listings', component: Listings, pathMatch: 'full'},
     {path: 'listings/:id', component: ListingDetail},
     {path: 'contact/:id', component: Contact},
